4a2fe835df304681e596e9ecd1423a8741ce9f0b
terp <andrewterp21@gmail.com>
2025-01-02 17:19:34 -0500

    import packages into main

--- src/main.py
+++ src/main.py
@@ -1,2 +1,23 @@
 import numpy as np
 import pandas as pd
+import datetime
+from datetime import date
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt, numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import AgglomerativeClustering
+from matplotlib.colors import ListedColormap
+from sklearn import metrics
+import warnings
+import sys
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+np.random.seed(42)

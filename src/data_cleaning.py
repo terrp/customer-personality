e879844f3e2342f2cf9474dd531e2a46a2ed9b65
terp <andrewterp21@gmail.com>
2025-01-04 19:54:07 -0500

    Cleaned up columns for readability

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,5 +4,12 @@ def load_and_clean_data(filepath):
     data = pd.read_csv(filepath, sep="\t")
     data = data.dropna()
     data["Dt_Customer"] = pd.to_datetime(data["Dt_Customer"], dayfirst=True)
+    data = data.rename(columns={
+        "MntWines": "Wines",
+        "MntFruits":"Fruits",
+        "MntMeatProducts":"Meat",
+        "MntFishProducts":"Fish",
+        "MntSweetProducts":"Sweets",
+        "MntGoldProds":"Gold"})
     return data
 

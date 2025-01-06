5c02871a9637d1a3b21421da80e752835ed13db8
terp <andrewterp21@gmail.com>
2025-01-06 11:09:41 -0500

    Saves relative_plot as png file now

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -7,11 +7,15 @@ sns.set_theme(rc={"axes.facecolor":"#FFF9ED","figure.facecolor":"#FFF9ED"})
 pallet = ["#682F2F", "#9E726F", "#D6B2B1", "#B9C0C9", "#9F8A78", "#F3AB60"]
 cmap = colors.ListedColormap(["#682F2F", "#9E726F", "#D6B2B1", "#B9C0C9", "#9F8A78", "#F3AB60"])
 
-# Plot Income, Age, Recency, Spent, Family_Size, and Shopper_Length
+# Plot Income, Age, Recency, Spent, Family_Size, Is_Parent and Shopper_Length
 def relative_plot(data):
-    To_Plot = ["Income", "Recency", "Family_Size", "Age", "Spent", "Shopper_Length"]
+    data["Is_Parent"] = data["Is_Parent"].astype("category")
+    To_Plot = ["Income", "Recency", "Family_Size", "Age", "Spent", "Shopper_Length", "Is_Parent"]
     print("Relative mapping of possibly related features: Data Subset")
     plt.figure()
     sns.pairplot(data[To_Plot], hue= "Is_Parent",palette= (["#682F2F","#F3AB60"]))
-    #Taking hue 
+    
+    # Save and display plot to png
+    plt.savefig("relative_plot.png", dpi=300, bbox_inches="tight")
+    print("Plot saved as 'relative_plot.png'.") 
     plt.show()

8d0255fe25e02714a642c35b1925d96576eaaf4e
terp <andrewterp21@gmail.com>
2025-01-07 17:24:31 -0500

    Created scaling function for dataset

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,5 +1,6 @@
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 def label_encode(data):
     # Change categorical variables to numeric
@@ -13,3 +14,16 @@ def label_encode(data):
         data[i] = LE.fit_transform(data[i])
     return data
 
+def scale_data(data):
+    # Copy the data 
+    data_copy = data.copy()
+    # Drop columns on deals accepted and complaints
+    cols_del = ['AcceptedCmp3', 'AcceptedCmp4', 'AcceptedCmp5', 'AcceptedCmp1','AcceptedCmp2', 'Complain', 'Response']
+    data_copy.drop(cols_del, axis = 1, inplace= True)
+
+    # Scaling
+    scaler = StandardScaler()
+    scaler.fit(data_copy)
+    scaled_ds = pd.DataFrame(scaler.transform(data_copy), columns= data_copy.columns)
+
+    return scaled_ds
